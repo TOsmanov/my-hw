ad2501c8eaf620a955f9f6993e72df22fd7aaba1
Timur Osmanov <timurosm@yandex.ru>
2023-12-26 22:22:22 +0300

    fix misstakes

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -14,10 +14,10 @@ func main() {
 	var width int
 	var height int
 
-	fmt.Printf("To display a chessboard with equal sides, enter the size of the side.\n")
-	fmt.Printf("Example of a square chessboard with side eight: 8\n")
-	fmt.Printf("To display a rectangular chessboard, enter the dimensions of the sides in the format: <width>x<height>\n")
-	fmt.Printf("Example of a 10 by 6 chessboard: 10x6\n")
+	fmt.Println("To display a chessboard with equal sides, enter the size of the side.")
+	fmt.Println("Example of a square chessboard with side eight: 8")
+	fmt.Println("To display a rectangular chessboard, enter the dimensions of the sides in the format: <width>x<height>")
+	fmt.Println("Example of a 10 by 6 chessboard: 10x6")
 	fmt.Scanln(&input)
 
 	if len(input) == 0 {
@@ -27,14 +27,14 @@ func main() {
 	inputArr := strings.Split(strings.ToLower(input), "x")
 
 	width, err = strconv.Atoi(inputArr[0])
-	if width <= 2 {
+	if width < 2 {
 		width = 2
 	}
 	if len(inputArr) < 2 {
 		height = width
 	} else {
 		height, err = strconv.Atoi(inputArr[1])
-		if height <= 2 {
+		if height < 2 {
 			height = 2
 		}
 	}
