9091f45c12298e65cbec149b4371131c4a59a1ed
Timur Osmanov <timurosm@yandex.ru>
2024-01-29 20:17:50 +0300

    fix lint error

--- hw07_word_counter/counter/pkg.go
+++ hw07_word_counter/counter/pkg.go
@@ -9,26 +9,10 @@ func CountWords(text string) map[string]int {
 	word := make([]rune, 0, 40)
 	output := make(map[string]int)
 	for i, char := range chars {
-		if i != len(chars)-1 {
+		switch {
+		case i == len(chars)-1:
 			switch char {
-			case 32, 9, 10:
-				if len(word) >= 1 {
-					w := strings.ToLower(string(word))
-					val, ok := output[w]
-					if ok {
-						output[w] = val + 1
-					} else {
-						output[w] = 1
-					}
-				}
-				word = make([]rune, 0, 40)
-			case 58, 59, 42, 44, 45, 46, 33, 63:
-			default:
-				word = append(word, char)
-			}
-		} else {
-			switch char {
-			case 32, 9, 10, 33, 42, 44, 45, 46, 58, 59, 63:
+			case 9, 10, 32, 33, 42, 44, 45, 46, 58, 59:
 			default:
 				word = append(word, char)
 			}
@@ -41,6 +25,21 @@ func CountWords(text string) map[string]int {
 					output[w] = 1
 				}
 			}
+		case char == 9, char == 10, char == 32:
+			if len(word) >= 1 {
+				w := strings.ToLower(string(word))
+				val, ok := output[w]
+				if ok {
+					output[w] = val + 1
+				} else {
+					output[w] = 1
+				}
+			}
+			word = make([]rune, 0, 40)
+		case char == 58, char == 59, char == 42, char == 44:
+		case char == 45, char == 46, char == 33, char == 63:
+		default:
+			word = append(word, char)
 		}
 	}
 	return output
