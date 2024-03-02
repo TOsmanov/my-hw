37ee2ca92064000c2c70669d14abb27b3fb02c0c
Timur Osmanov <timurosm@yandex.ru>
2024-03-02 22:52:43 +0300

    apply suggestions

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -8,46 +8,44 @@ import (
 )
 
 func Sensor(max int64, delay time.Duration) chan int {
-	start := time.Now()
-	stop := start.Add(delay)
 	c := make(chan int)
+	timeout := time.After(delay)
 	go func() {
+		defer close(c)
 		for {
-			r, err := rand.Int(rand.Reader, big.NewInt(max))
-			if err != nil {
-				break
-			}
-			c <- int(r.Int64())
-			if time.Now().After(stop) {
-				break
+			select {
+			case <-timeout:
+				return
+			default:
+				r, err := rand.Int(rand.Reader, big.NewInt(max))
+				if err != nil {
+					return
+				}
+				c <- int(r.Int64())
 			}
 		}
-		close(c)
 	}()
 	return c
 }
 
-func Reader(l int, inputCh chan int) chan int {
+func Reader(depth int, inputCh chan int) chan int {
 	c := make(chan int)
-	container := make([]int, 0, l)
+	counter := 0
+	storage := 0
 	go func() {
-		for {
-			a, ok := <-inputCh
-			if !ok {
-				break
-			}
-			container = append(container, a)
-			if len(container) == l {
-				var mid float64
-				for i := 0; i < l; i++ {
-					mid += float64(container[i])
-				}
-				mid = mid/float64(l) + 0.5
+		defer close(c)
+		for a := range inputCh {
+			switch {
+			case counter == depth:
+				mid := float32(storage/depth) + 0.5
 				c <- int(mid)
-				container = make([]int, 0, l)
+				counter = 0
+				storage = 0
+			default:
+				storage += a
+				counter++
 			}
 		}
-		close(c)
 	}()
 	return c
 }
@@ -56,19 +54,7 @@ func main() {
 	emulationSensor := Sensor(1000, time.Minute)
 	readData := Reader(10, emulationSensor)
 
-wait:
-	for {
-		select {
-		case _, ok := <-emulationSensor:
-			if !ok {
-				fmt.Println("The processing of the motion sensor data is completed.")
-				break wait
-			}
-		case output, ok := <-readData:
-			if !ok {
-				break wait
-			}
-			fmt.Println(output)
-		}
+	for output := range readData {
+		fmt.Println("readData", output)
 	}
 }
